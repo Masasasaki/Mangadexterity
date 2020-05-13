2773115a8b0cc6146244aabe6848c9f866358fc7
Masaki <masakiasanuma@outlook.com>
2020-05-13 00:20:40 -0700

    Tested mangadex api functions

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const mangadex = require('mangadex-api')
 app.get('/', (req, res) => {
     mangadex.getManga(29298).then(({ manga, chapter }) => {
         const info = `Manga ${manga.title} has ${chapter.length} chapters!`
-        res.send(info)
+        chapter = chapter.filter(ch => ch.lang_name === 'English')
+        mangadex.getChapter(chapter[0].id).then(chap => {
+            console.log(chap.page_array[0])
+            res.send(chap.page_array[0])
+        })
     })
 })
 
